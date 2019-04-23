64a0344a5571d53b739405a52dc174b4d99a147f
jordan <jcs5317@gmail.com>
2019-04-23 16:52:03 -0700

    setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//timer count down to end of game
+window.onload = function() {
+
+};
+//set number counter to 100
+var number = 100;
+
+//var that will store interval ID when game starts
+var intervalId;
