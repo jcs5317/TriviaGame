a63ad230204835c3d2dc32b28bcc3d9ceac8cad9
jordan <jcs5317@gmail.com>
2019-04-24 13:16:45 -0700

    timer needed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
-//timer count down to end of game
+/*timer count down to end of game
 window.onload = function() {
 
-};
+};*/
 //set number counter to 100
-var number = 100;
+var counter = 0;
+var timeleft = 100;
 
+function convertSeconds(s) {
+    var min = floor(s / 60);
+    var sec = s % 60;
+}
+/*function setup() {
+    noCanvas();
+}*/
+
+var timer = select('#timer');
+timer.html(convertSeconds(timeleft - counter));
+
+function timeIt(){
+    counter++;
+    timer.html(convertSeconds(timeLeft - counter));
+}
+setInterval(timeIt, 1000);
 //var that will store interval ID when game starts
 var intervalId;
